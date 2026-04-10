9d0e243d1b45408eb5f16d604577c00bcbb6452b
KSUSISS <mr.bezzliki@mail.ru>
2026-04-10 09:36:19 +0300

    Refactor Calculator methods to use block syntax

--- src/Module.Core/Calculator.cs
+++ src/Module.Core/Calculator.cs
@@ -2,17 +2,26 @@ namespace Module.Core
 {
     public class Calculator
     {
-        public double Add(double a, double b) => a + b;
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
 
-        public double Subtract(double a, double b) => a - b;
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
 
-        public double Multiply(double a, double b) => a * b;
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
 
         public double Divide(double a, double b)
         {
             if (b == 0)
             {
-                throw new System.DivideByZeroException("Division by zero is not allowed");
+                throw new DivideByZeroException("Division by zero is not allowed");
             }
             return a / b;
         }
