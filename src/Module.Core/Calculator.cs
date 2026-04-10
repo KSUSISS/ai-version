0b191070b1353c59a46a240cf1566e0fd346f3ce
KSUSISS <mr.bezzliki@mail.ru>
2026-04-10 09:34:19 +0300

    Implement basic calculator operations

--- src/Module.Core/Calculator.cs
+++ src/Module.Core/Calculator.cs
@@ -1,1 +1,20 @@
+namespace Module.Core
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) => a + b;
 
+        public double Subtract(double a, double b) => a - b;
+
+        public double Multiply(double a, double b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new System.DivideByZeroException("Division by zero is not allowed");
+            }
+            return a / b;
+        }
+    }
+}
